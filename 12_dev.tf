2e664621f47166380135bb6a62d8e75f8f17ed6d
deepaSadhwani <31128715+deepaSadhwani@users.noreply.github.com>
2021-07-22 16:45:30 +0530

    Created Role script

---
+++ 12_dev.tf
@@ -0,0 +1,9 @@
+module "project-custom-role" {
+source  	= "mahesh-madipally/db_Role_template/google"
+target_level            	= "project"
+target_id            	= "db-sample-314713"
+role_id            	= "12_dev"
+title              	= "sample"
+description  = 	= "test"
+permissions  = 	= "[iam.roles.list, iam.roles.create]" 
+ }
