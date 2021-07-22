75c770ae1a2eac44f2c52c55bd36ba4000c3a05f
deepaSadhwani <31128715+deepaSadhwani@users.noreply.github.com>
2021-07-22 16:55:25 +0530

    Created Role script

---
+++ 22_dev.tf
@@ -0,0 +1,9 @@
+module "project-custom-role" {
+source  			= "mahesh-madipally/db_Role_template/google"
+target_level     = "project"
+target_id        = "db-sample-314713"
+role_id          = "22_dev"
+title            = "test"
+description 		= "123"
+permissions 		=java.util.stream.ReferencePipeline$3@2e67fc7d 
+ }
