773897dae0c38f02c3f5f9706aca57f903f21be7
deepaSadhwani <31128715+deepaSadhwani@users.noreply.github.com>
2021-07-23 13:41:46 +0530

    updated Role script

--- string.tf
+++ string.tf
@@ -1,9 +1,9 @@
 module "project-custom-role" {
 source  			= "mahesh-madipally/db_Role_template/google"
-target_level     = "string"
-target_id        = "string"
-role_id          = "string"
-title            = "string"
-description 		= "string"
-permissions 		=["string"] 
+target_level     = "project"
+target_id        = "test"
+role_id          = "dev"
+title            = "null"
+description 		= "test"
+permissions 		=["123", "22"] 
  }
