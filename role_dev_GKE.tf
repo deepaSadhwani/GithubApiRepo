6dd9845dfefa937f87451898aeac61f3658432d6
deepaSadhwani <31128715+deepaSadhwani@users.noreply.github.com>
2021-08-05 17:35:44 +0530

    Update role_dev_GKE.tf

--- role_dev_GKE.tf
+++ role_dev_GKE.tf
@@ -1,9 +1,1 @@
-module = "project-custom-role"{
-source  			= "mahesh-madipally/db_Role_template/google"
-target_level     = "project"
-target_id        = "db-sample-314713"
-role_id          = "role_dev_12"
-title            = "role_dev_GKE"
-description 		= "Custom Role Description"
-permissions 		=["iam.roles.list", "iam.roles.create"] 
- }
+
