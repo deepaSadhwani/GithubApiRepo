027a45731942079cc1802e84e7f6c264719401fd
deepaSadhwani <31128715+deepaSadhwani@users.noreply.github.com>
2021-08-05 18:31:48 +0530

    Updated Role script

--- predefined_role_dev_GKE.tf
+++ predefined_role_dev_GKE.tf
@@ -5,9 +5,7 @@ target_id        = "db-sample-314713"
 role_id          = "role_dev_predef_GKE"
 title            = "role_dev_predef_GKE"
 description 		= "Predefined Role Description"
-base_roles       =["roles/iam.serviceAccountAdmin"] 
- }
-permissions 		=["iam.roles.list", "iam.roles.create"] 
- }
+base_roles       =["roles/iam.serviceAccountAdmin"]
+permissions 		=["iam.roles.list", "iam.roles.create"]
 excluded_permissions =  		=["iam.serviceAccounts.setIamPolicy"] 
  }
